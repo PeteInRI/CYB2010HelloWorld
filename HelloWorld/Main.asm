d5ec54c8ff70163db526d07f50b81cacd67f0bbf
PReid <PReid@PVD23044DL>
2023-08-18 10:34:14 -0400

    Add project files.

---
+++ HelloWorld/Main.asm
@@ -0,0 +1,18 @@
+; Project Name:
+; Author:
+
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess proto,dwExitCode:dword
+
+.data
+
+.code
+main proc
+
+			
+
+	invoke ExitProcess,0
+main endp
+end main
